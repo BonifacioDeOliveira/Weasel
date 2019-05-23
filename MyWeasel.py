4ee1fbd786b7231006ddec8ac33f2cac91817029
Bonifacio <bonifaciodeoliveira@outlook.com>
2019-05-23 14:26:17 -0400

    GUI

--- MyWeasel.py
+++ MyWeasel.py
@@ -4,14 +4,14 @@ import string
 
 def genRandStr(CONST_STR):   # print random string of length CONST_STR
     """Return random string of length CONST_STR."""
-    randStr = ''
+    ENTRADA  = ''
     for x in range(0, len(CONST_STR)):
-        randStr = randStr + random.choice(string.ascii_uppercase + string.whitespace)
-    return randStr
+        ENTRADA  = ENTRADA  + random.choice(string.ascii_uppercase + string.whitespace)
+    return ENTRADA
 
 
 def compStr(randStr,CONST_STR):   # compare CONST_STR with randStr
-    """Return index of randStr -- number of common characters in randStr and CONST_STR."""
+    """Return index of ENTRADA  -- number of common characters in ENTRADA  and CONST_STR."""
     randStrIndex = 0
     for i in range(0, len(CONST_STR)):
         if CONST_STR[i] == randStr[i]:
@@ -19,8 +19,8 @@ def compStr(randStr,CONST_STR):   # compare CONST_STR with randStr
     return randStrIndex
 
 
-def modRandStr(randStr,CONST_STR,CONST_PROB):    # modify each character of randStr with probability CONST_PROB%
-    """Return modified randStr -- each character of randStr is modified with probability CONST_PROB%."""
+def modRandStr(randStr,CONST_STR,CONST_PROB):    # modify each character of ENTRADA  with probability CONST_PROB%
+    """Return modified ENTRADA  -- each character of ENTRADA  is modified with probability CONST_PROB%."""
     newRandStr = randStr
     for x in range(0, len(CONST_STR)):
         randInt = random.randint(1, 100)
@@ -29,27 +29,31 @@ def modRandStr(randStr,CONST_STR,CONST_PROB):    # modify each character of rand
     return newRandStr
 
 
-def Weasel():
+def Weasel(CONST_STR, SIZE, PROB, ENTRADA):
 
-    CONST_STR = "METHINKS IT IS LIKE A WEASEL"
-    CONST_SIZE = 100
-    CONST_PROB = 5
-
+    CONST_SIZE = int(SIZE)
+    CONST_PROB = int(PROB)   
     loopNumber = 0
     strFound = False
-    randStr = "cccccccccccccccccccccccccccc"
     resultList = []
     while not strFound:
         strList = [''] * CONST_SIZE
         indexList = [None] * CONST_SIZE
         for x in range(0, CONST_SIZE):
-            strList[x] = modRandStr(randStr,CONST_STR,CONST_PROB)
+            strList[x] = modRandStr(ENTRADA,CONST_STR,CONST_PROB)
             indexList[x] = compStr(strList[x],CONST_STR)
         maxIndex = max(indexList)
         if maxIndex == len(CONST_STR):
             strFound = True
-        randStr = strList[indexList.index(maxIndex)]
-        resultList.append(str(loopNumber) + ": " + randStr + " -- score: " + str(maxIndex))
+        ENTRADA  = strList[indexList.index(maxIndex)]
+        resultList.append(str(loopNumber) + ": " + ENTRADA  + " -- score: " + str(maxIndex))
         loopNumber += 1
     return resultList
+
+'''
+CONST_STR = "METHINKS IT IS LIKE A WEASEL"
+CONST_SIZE = 100
+CONST_PROB = 5
+ENTRADA  = "cccccccccccccccccccccccccccc"
+'''
 
--- main.py
+++ main.py
@@ -1,8 +1,97 @@
+import os
 from tkinter import *
+import MyWeasel as MW
 
-class Application:
+class App:
+
     def __init__(self, master=None):
-        pass
+
+        self.fontePadrao = ("Arial", "10")
+
+        self.primeiroContainer = Frame(master)
+        self.primeiroContainer["padx"] = 10
+        self.primeiroContainer.pack()
+
+        self.segundoContainer = Frame(master)
+        self.segundoContainer["pady"] = 0
+        self.segundoContainer.pack()
+
+        self.terceiroContainer = Frame(master)
+        self.terceiroContainer["pady"] = 0
+        self.terceiroContainer.pack()
+
+        self.quartoContainer = Frame(master)
+        self.quartoContainer["pady"] = 0
+        self.quartoContainer.pack()
+
+        self.quintoContainer = Frame(master)
+        self.quintoContainer["pady"] = 0
+        self.quintoContainer.pack()
+
+        self.sextoContainer = Frame(master)
+        self.sextoContainer["pady"] = 10
+        self.sextoContainer.pack()
+
+        self.EntradaLabel = Label(self.primeiroContainer,text="Entrada", font=self.fontePadrao)
+        self.EntradaLabel.pack(side=LEFT)
+
+        self.Entrada = Entry(self.primeiroContainer)
+        self.Entrada["width"] = 30
+        self.Entrada["font"] = self.fontePadrao
+        self.Entrada.pack(side=LEFT)
+
+        self.CopiasLabel = Label(self.segundoContainer,text="Strings Geradas", font=self.fontePadrao)
+        self.CopiasLabel.pack(side=LEFT)
+
+        self.Copias = Entry(self.segundoContainer)
+        self.Copias["width"] = 30
+        self.Copias["font"] = self.fontePadrao
+        self.Copias.pack(side=LEFT)
+
+        self.mutacaoLabel = Label(self.terceiroContainer,text="Taxa de Mutacao", font=self.fontePadrao)
+        self.mutacaoLabel.pack(side=LEFT)
+
+        self.mutacao = Entry(self.terceiroContainer)
+        self.mutacao["width"] = 30
+        self.mutacao["font"] = self.fontePadrao
+        self.mutacao.pack(side=LEFT)
+
+        self.targetLabel = Label(self.quartoContainer,text="Target", font=self.fontePadrao)
+        self.targetLabel.pack(side=LEFT)
+
+        self.target  = Entry(self.quartoContainer)
+        self.target["width"] = 30
+        self.target["font"] = self.fontePadrao
+        self.target.pack(side=LEFT)
+
+        self.iniciar = Button(self.quintoContainer)
+        self.iniciar["text"] = "Iniciar !"
+        self.iniciar["width"] = 12
+        self.iniciar["command"] = (lambda : resultado(self.Entrada.get(), self.Copias.get(), self.mutacao.get(), self.target.get()))
+        #self.iniciar["command"] = (lambda : resultado())
+        self.iniciar.pack()
+       
+        self.scrollbar = Scrollbar(self.sextoContainer) 
+        self.scrollbar.pack( side = RIGHT, fill = Y ) 
+
+        self.mylist = Listbox(self.sextoContainer, yscrollcommand = self.scrollbar.set, ) 
+        self.mylist.pack( side = LEFT, fill = BOTH ) 
+        self.mylist["width"] = 200
+        self.mylist["height"] = 400
+        self.scrollbar.config( command = self.mylist.yview )
+
+        #self.listar.pack(side=UP)
+        
+        def resultado(CONST_STR, CONST_SIZE, CONST_PROB, ENTRADA):
+            #CONST_STR = "METHINKS IT IS LIKE A WEASEL"
+            #CONST_SIZE = 100
+            #CONST_PROB = 5
+            #ENTRADA  = "cccccccccccccccccccccccccccc"
+            for i in (MW.Weasel(ENTRADA, CONST_SIZE, CONST_PROB, CONST_STR)):
+                self.mylist.insert(END, str(i))
+                
 root = Tk()
-Application(root)
+root.geometry("600x600")
+root.title("Weasel")
+App(root)
 root.mainloop()
